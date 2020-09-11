9ff2ad8b9cb73b59d0041e436c7184f8591df949
Work <work@Holliss-MBP.lan>
2020-09-11 09:09:59 -0500

    landing page template

--- src/templates/landing-template.js
+++ src/templates/landing-template.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import PageComponents from "../components/PageComponents"
 
 const LandingTemplate = ({data}) => {
   const doc = data.datoCmsLanding
@@ -12,6 +13,7 @@ const LandingTemplate = ({data}) => {
     <Layout>
       <SEO title={doc.teaser}  />
       <Hero teaser={doc.teaser}  headline={doc.headline}/>
+      <PageComponents components={doc.components} />
     </Layout>
   )
 }
@@ -21,8 +23,76 @@ export default LandingTemplate
 export const query = graphql`
 query($slug: String!) {
     datoCmsLanding(slug: {eq: $slug}) {
-        teaser
-        headline
+      headline
+      teaser
+      shortDescription
+      components {
+        ... on DatoCmsContentBlock {
+          id
+          splitContent
+          sticky
+          teaser
+          content
+          contentPosition
+          model {
+            name
+          }
+          media {
+            fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+              ...GatsbyDatoCmsFluid
+            }
+            isImage
+            filename
+            alt
+            title
+          }
+        }
+        ... on DatoCmsGridList {
+          id
+          headline
+          darkBackground
+          listing
+          model {
+            name
+          }
+        }
+        ... on DatoCmsFeaturedWork {
+          id
+          headline
+          model {
+            name
+          }
+          work {
+            id
+            shortDescription
+            teaser
+            slug
+            thumbnail {
+              fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+                ...GatsbyDatoCmsFluid
+              }
+              isImage
+              filename
+              alt
+              title
+            }
+          }
+        }
+        ... on DatoCmsCtaBlock {
+          id
+          teaser
+          headline
+          description
+          model {
+            name
+          }
+          link {
+            ... on DatoCmsContact {
+              slug
+            }
+          }
+        }
+      }
     }
 }
 `
